c188552e82d1e59b1cb377f63e2c55b2e4936285
BenMoreira <bmoreira@ggc.edu>
2023-06-28 19:59:47 -0400

    Adds more three.js libraries and add card template

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,36 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useRef, useState } from 'react';
+import { Canvas, useFrame } from '@react-three/fiber';
+import { useSpring, a } from '@react-spring/web';
+import './App.css';
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
+  const [flipped, setFlipped] = useState(false);
+
+  const { transform, opacity } = useSpring({
+    opacity: flipped ? 1 : 0,
+    transform: `perspective(600px) rotateX(${flipped ? 180 : 0}deg)`,
+    config: { mass: 5, tension: 500, friction: 80 },
+  });
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    <div className="w-full h-screen flex justify-center items-center" onClick={() => setFlipped(!flipped)}>
+      <a.div
+        className="max-w-[500px] max-h-[500px] w-[350px] h-[200px] cursor-pointer will-change-transform  
+         bg-[url(https://images.unsplash.com/photo-1544511916-0148ccdeb877?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&w=1901&q=80i&auto=format&fit=crop)]
+         bg-cover"
+        style={{ opacity: opacity.to(o => 1 - o), transform }}
+      />
+      <a.div
+        className="absolute max-w-[500px] max-h-[500px] w-[350px] h-[200px] cursor-pointer will-change-transform
+         bg-[url(https://images.unsplash.com/photo-1540206395-68808572332f?ixlib=rb-1.2.1&w=1181&q=80&auto=format&fit=crop)] bg-cover"
+        style={{
+          opacity,
+          transform,
+          rotateX: '180deg',
+        }}
+      />
+    </div>
+  );
+};
 
-export default App
+export default App;
